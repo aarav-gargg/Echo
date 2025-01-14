1e75ee316909326cd98aa825a2bdc246e437c7d7
aarav-gargg <aaravgarg975@gmail.com>
2025-01-14 13:20:34 +0530

    finishing with the user dashboard

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -12,7 +12,9 @@ import { useForm } from "react-hook-form";
 import Link from 'next/link'
 import { Button } from '@/components/ui/button'
 import { Separator } from "@/components/ui/separator";
-import { LogOut , Copy } from 'lucide-react'
+import { LogOut, Copy, Loader, RefreshCcw } from 'lucide-react'
+import { Switch } from "@/components/ui/switch";
+import MessageCard from "@/components/MessageCard";
 
 const Dahsboard = () => {
 
@@ -110,8 +112,8 @@ const Dahsboard = () => {
   const handleCopyUrl = () => {
     navigator.clipboard.writeText(profileUrl);
     toast({
-      title : "Url Copied" , 
-      description : "Profile url has been copied to the clipboard"
+      title: "Url Copied",
+      description: "Profile url has been copied to the clipboard"
     })
   }
 
@@ -137,27 +139,79 @@ const Dahsboard = () => {
         <h1 className="text-4xl font-bold mb-4">
           User Dashboard
         </h1>
-        <div className="my-8 md:mx-8 lg:mx-auto p-6 bg-white rounded w-full max-w-6xl">
+        <div className="my-6 md:mx-8 lg:mx-auto p-6 bg-white rounded w-full max-w-6xl">
           <h1 className="font-extrabold text-2xl mb-3">
             Unleash the mystery! Share your link, spark curiosity, and let the secrets roll in.
           </h1>
           <div className="my-2">
             <h2 className="text-lg font-semibold mb-2">Copy Your Link from here</h2>
             <div className="flex flex-row">
-            <input type="text"
-              disabled
-              value={profileUrl}
-               className="input input-bordered w-full p-2 mr-2 bg-gray-100 text-black font-semibold rounded-xl"
-            />
-            <Button variant= "outline" onClick={handleCopyUrl}>
-              <Copy/> Copy
-            </Button>
+              <input type="text"
+                disabled
+                value={profileUrl}
+                className="input input-bordered w-full p-2 mr-2 bg-gray-100 text-black font-semibold rounded-xl"
+              />
+              <Button variant="outline" onClick={handleCopyUrl}>
+                <Copy /> Copy
+              </Button>
+            </div>
+            <div className="my-3 flex flex-row items-center gap-2">
+              <Switch
+                {...register('acceptingMessages')}
+                checked={acceptingMessages}
+                onCheckedChange={handleSwitchChange}
+                disabled={isSwitchLoading}
+              />
+              <span className="font-semibold text-lg font-serif">
+                Accept Messages : <span className={`${acceptingMessages ? "text-green-600" : "text-red-600"} font-bold`}>{acceptingMessages ? "On" : "Off"}</span>
+              </span>
             </div>
           </div>
         </div>
 
-        <Separator/>
-        
+        <Separator />
+
+        <div className="my-5">
+          <div className="mt-4 flex flex-row justify-between">
+            <div>
+              <h1 className="text-4xl font-extrabold">Recieved Messages</h1>
+            </div>
+            <div>
+              <Button onClick={(e) => {
+                e.preventDefault();
+                fetchMessages(true);
+              }}>
+                {isLoading ? (
+                  <>
+                    <Loader className="animate-spin" />
+                    Refreshing
+                  </>
+                ) : (
+                  <>
+                    <RefreshCcw />
+                    Refresh
+                  </>
+                )}
+              </Button>
+            </div>
+          </div>
+          <div className="my-6 flex items-center justify-center">
+            {messages.length > 0 ? (
+              messages.map((item, index) => (
+                <MessageCard
+                  key={index}
+                  message={item}
+                  onMessageDelete={handleDeleteMessage}
+                />
+              ))
+            ) : (
+              <div className="font-bold text-3xl text-red-700">
+                No Messages to Display.
+              </div>
+            )}
+          </div>
+        </div>
+
       </div>
     </>
   )
